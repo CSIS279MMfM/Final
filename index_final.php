7ab507c3ed3a9595e846b440ba2edca40a4b558b
AF072897 <Alex.Fitzmaurice@cerner.com>
2020-04-28 16:37:59 -0500

    Setup final form page - ready for data

--- index_final.php
+++ index_final.php
@@ -7,47 +7,91 @@
 
 <body>
     <main>
-    <h1>Registration Form</h1>
+    <h1>Student Registration Form</h1>
     <form action="display_results.php" method="post">
 
     <fieldset> 
-    <legend>Account Information</legend>
-        <label>E-Mail:</label>
-        <input type="text" name="email" value="" class="textbox">
+        <legend>Personal Info</legend>
+        
+        <h2>Student Info</h2>
+        <label>First Name:</label>
+        <input type="text" name="fname" value="" class="textbox">
         <br>
 
-        <label>Password:</label>
-        <input type="password" name="password" value="" class="textbox">
+        <label>Middle Name:</label>
+        <input type="text" name="mname" value="" class="textbox">
         <br>
 
-        <label>Phone Number:</label>
-        <input type="text" name="phone" value="" class="textbox">
+        <label>Last Name:</label>
+        <input type="text" name="lname" value="" class="textbox"><br><br>
+
+        <label>Gender:</label>
+        <select name="gender">
+            <option value="default" selected></option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+            <option value="none">Prefer Not to Say</option>
+        </select><br><br>
+
+        <label>Birthdate:</label>
+        <input type="date" name="birthday" value=""><br>
+
     </fieldset>
 
     <fieldset>
-    <legend>Settings</legend>
+        <legend>Contact Info</legend>
+        <label>Street Address 1:</label>
+        <input type="text" name="fname" value="" class="textbox">
 
-        <p>How did you hear about us?</p>
-        <input type="radio" name="heard_from" value="Search Engine">
-        Search engine<br>
-        <input type="radio" name="heard_from" value="Friend">
-        Word of mouth<br>
-        <input type=radio name="heard_from" value="Other">
-        Other<br>
+        <label>Street Address 2:</label>
+        <input type="text" name="mname" value="" class="textbox">
 
-        <p>Would you like to receive announcements about new products
-           and special offers?</p>
-        <input type="checkbox" name="wants_updates">YES, I'd like to receive
-        information about new products and special offers.<br>
+        <label>City:</label>
+        <input type="text" name="lname" value="" class="textbox">
 
-        <p>Contact via:</p>
-        <select name="contact_via">
-                <option value="email">Email</option>
-                <option value="text">Text Message</option>
-                <option value="phone">Phone</option>
-        </select>
+        <label>State/Province:</label>
+        <input type="text" name="lname" value="" class="textbox">
 
-        <p>Comments:</p>
+        <label>Postal/Zip Code:</label>
+        <input type="number" name="lname" value="" class="textbox">
+
+        <label>Country:</label><br>
+
+        <label>Student Email:</label>
+        <input type="email" name="email" placeholder="ex: myname@example.com"><br><br>
+
+        <label>Mobile Number:</label>
+        <input type="tel" name="mobile"><br>
+
+        <label>Work Number:</label>
+        <input type="tel" name="mobile"><br><br>
+
+        <label>Company:</label>
+        <input type="text" name="company"><br><br>
+    </fieldset>
+    <fieldset>
+        <legend>Course Info</legend>
+        <h3>Subject:</h3>
+        <select name="subjects">
+            <option value="default" selected></option>
+            <option value="csis">CSIS</option>
+            <option value="eng">ENG</option>
+            <option value="sci">SCI</option>
+            <option value="hist">HIST</option>
+        </select><br><br>
+
+        <h3>Courses:</h3>
+        <select name="courses">
+            <?php foreach ($courses as $course) : ?>
+                <option value="?action=view_product&amp;product_id=<?php 
+                          echo $course['course_num']; ?>">
+                    <?php echo $course['course_name']; ?>
+                </option>
+            <?php endforeach; ?>
+        </select>   
+
+        <h3>Comments:</h3>
         <textarea name="comments" rows="4" cols="50"></textarea>
     </fieldset>
 
